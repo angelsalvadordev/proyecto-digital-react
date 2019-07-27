fd3ec5da4a10b82c262775d43dfdc4b969cf0c84
Angel Salvador <angelsalvadordev@gmail.com>
2019-07-26 19:45:51 -0500

    update

--- src/components/molecules/FormContact.jsx
+++ src/components/molecules/FormContact.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-const FormContact = () => {
-    return (
-        <form action="#" className="contact-form">
-            <input className="contact-input" type="text" placeholder="Nombre (requerido)" />
-            <input className="contact-input" type="text" placeholder="Tu email (requerido)" />
-            <input className="contact-input" type="text" placeholder="Asunto" />
-            <div className="contact-message">
-                <textarea className="contact-input contact-textarea f-poppins-r" type="text" placeholder="Mensaje (requerido)"></textarea>
-                <input className="contact-input contact-button f-icon" type="submit" value="send" />
-            </div>
-        </form>
-    )
+class FormContact extends React.Component {
+    render() {
+        return (
+            <form action="#" className="contact-form">
+                <input className="contact-input" type="text" placeholder="Nombre (requerido)" />
+                <input className="contact-input" type="text" placeholder="Tu email (requerido)" />
+                <input className="contact-input" type="text" placeholder="Asunto" />
+                <div className="contact-message">
+                    <textarea className="contact-input contact-textarea f-poppins-r" type="text" placeholder="Mensaje (requerido)"></textarea>
+                    <input className="contact-input contact-button f-icon" type="submit" value="send" />
+                </div>
+            </form>
+        )
+    }
 }
 
 export default FormContact
